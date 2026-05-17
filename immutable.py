7d2cc8294892875af744d40ac6c93f541326d95b
suhani1301-2005 <suhanisaxena014@gmail.com>
2026-05-17 21:02:11 +0530

    Delete immutable.py

--- immutable.py
+++
@@ -1,8 +0,0 @@
-#Why all fundamental datatypes are immutable?
-math = 50
-chem = 50 
-phy = 50 
-print(id(math))
-print(id(chem))
-print(id(phy))
-#All adresses are same because all the values are same and immutable datatypes are stored in the same memory location if they have the same value.
